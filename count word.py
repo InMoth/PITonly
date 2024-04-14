0a76fbdee2d47664f578a0626c025abc700c9d1d
Shestakov <andrey.shestakov2004@mail.ru>
2024-04-15 01:45:21 +0700

    создал функцию main, которая использует модуль argparse для создания аргументов командной строки. При запуске программа ожидает строковый аргумент --text, который представляет собой кусок параграфа. Затем программа вызывает функцию count_unique_words, чтобы подсчитать количество уникальных слов в тексте и выводит результат на экран.

--- count word.py
+++ count word.py
@@ -6,4 +6,17 @@ def count_unique_words(text):
     return len(unique_words)
 
 
+def main() -> None:
+    parser = argparse.ArgumentParser(description="счетчик уникальных слов в тексте")
+    parser.add_argument("--text", type=str, help="получает на вход кусок параграфа")
+
+    args = parser.parse_args()
+
+    if args.text:
+        print(f"Количество уникальных слов в тексте: {count_unique_words(args.text)}")
+
+
+if __name__ == "__main__":
+    main()
+
 
